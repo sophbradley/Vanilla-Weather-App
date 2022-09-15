7c05b7c9027f3c2b7a2ed12daea3679776bbfc8b
sophbradley <111533096+sophbradley@users.noreply.github.com>
2022-09-15 18:24:10 +0100

    Added function to get coordinates and integrate to API URL, added API call

--- src/app.js
+++ src/app.js
@@ -42,6 +42,28 @@ function formattedDate(timestamp) {
   let day = days[dateTime.getDay()];
   return `${day} ${date} ${month} ${hours}:${minutes}`;
 }
+function displayForecast(response) {
+  console.log(response.data.daily);
+  let forecastDataElement = document.querySelector("#forecast-data");
+  let forecastHTML = `<div class="row justify-content-center forecast-data">`;
+  let days = ["Wed", "Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="col-2 "><span class="forecast-day">${day}</span>
+  <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt=""/>
+  <span class="forecast-max">19°</span>|<span class="forecast-min">14°</span>
+</div>`;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastDataElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "a43564c91a6c605aeb564c9ed02e3858";
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 
 function showTemperature(response) {
   let temperature = response.data.main.temp;
@@ -75,6 +97,8 @@ function showTemperature(response) {
 
   let timeUpdated = document.querySelector("#last-update");
   timeUpdated.innerHTML = formattedDate(response.data.dt * 1000);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -107,22 +131,6 @@ function displayFahrenheit(event) {
   tempElement.innerHTML = Math.round(fahrenheitTemp);
 }
 
-function forecastData() {
-  let forecastDataElement = document.querySelector("#forecast-data");
-  let forecastHTML = `<div class="row justify-content-center forecast-data">`;
-  let days = ["Wed", "Thu", "Fri", "Sat", "Sun"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `<div class="col-2 "><span class="forecast-day">${day}</span>
-  <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt=""/>
-  <span class="forecast-max">19°</span>|<span class="forecast-min">14°</span>
-</div>`;
-  });
-  forecastHTML = forecastHTML + `</div>`;
-  forecastDataElement.innerHTML = forecastHTML;
-}
-
 let celsiusTemp = null;
 
 let celsius = document.querySelector("#celsius");
@@ -135,4 +143,3 @@ let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
 search("London");
-forecastData();
